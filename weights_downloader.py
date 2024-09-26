b9ff4d94eee7e1dcce9004f8e7d5f0789e9bba0c
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-09-26 17:07:31 -0400

    Update weights_downloader.py

--- weights_downloader.py
+++ weights_downloader.py
@@ -60,9 +60,7 @@ class WeightsDownloader:
 
         print(f"⏳ Downloading {weight_str} to {dest}")
         start = time.time()
-        subprocess.check_call(
-            ["pget", "--log-level", "warn", "-xf", url, dest], close_fds=False
-        )
+        os.system(f"wget {url} -O {dest}")
         elapsed_time = time.time() - start
         try:
             file_size_bytes = os.path.getsize(
